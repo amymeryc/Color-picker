4874d417830863cb279d5a3d94338c5e7c87d1cf
Amy <amymerydesigns@gmail.com>
2026-03-02 23:00:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/color-picker/', 
-    
+    base: '/color-picker/', //
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
